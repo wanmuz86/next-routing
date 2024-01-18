89ab7c95ba505d8910bdc8d7162b55f27d0164c2
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-18 09:50:06 +0700

    first commit

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export default function ProfileUser() {
+// The parameter is retrieved from the URL through props
+// In the second part, we speciy the data type of the props
+export default function ProfileUser({params}:{params:{username:String}}) {
   return (
     <div>
         <h1>Profile with parameter page</h1>
-        <p>This my profile with parameter page</p>
+        <p>This my profile with {params.username}</p>
     </div>
   )
 }
