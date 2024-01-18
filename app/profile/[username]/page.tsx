4a5c957949dd8e2df1f2fe8d2b3905678dc2b756
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-18 09:47:43 +0700

    first commit

---
+++ app/profile/[username]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ProfileUser() {
+  return (
+    <div>
+        <h1>Profile with parameter page</h1>
+        <p>This my profile with parameter page</p>
+    </div>
+  )
+}
