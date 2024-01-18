fdd4a6a927b1865b1ebbd72bb9ddac07998bd72f
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-18 13:26:15 +0700

    not found and loading in review

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,12 @@
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 export default function ReviewDetail({
     params}:{params:{productId:string,reviewId:string}}) {
+      if (parseInt(params.reviewId)> 1000){
+        notFound();
+      }
+ 
   return (
     <div>
         <h1>Review Detail</h1>
