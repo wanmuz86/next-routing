2ff44f1c1aadc9947318ec8d98dee0f462c7244f
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-18 14:03:08 +0700

    nav link

--- app/(auth)/(with-auth-layout)/layout.tsx
+++ app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,10 +1,30 @@
+import Link from "next/link";
 export default function AuthLayout({
     children, // will be a page or nested layout
   }: {
     children: React.ReactNode
   }) {
+    const navLinks = [
+      {"name":"Register", href:"/register"},
+      {"name":"Login", href:"/login"},
+      {"name":"Forgot Password", href:"/forgot-password"},
+    ]
     return (
       <div>
+        <ul>
+        {
+          navLinks.map((link)=>{
+            return (
+              <li key={link.name}>
+                <Link href={link.href} >
+                {link.name}
+              </Link>
+               </li>
+            )
+          })
+          
+        }
+        </ul>
    
         {children}
         <h6>We are waiting for you</h6>
