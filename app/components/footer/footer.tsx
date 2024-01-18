ad25ed2915721730983d678e31e7b0bf90f7addb
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-18 10:45:02 +0700

    first commit

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div><p> Copyright 2024</p></div>
+  )
+}
---
+++ app/components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <h1>NextJS Demo</h1>
+    </div>
+  )
+}
