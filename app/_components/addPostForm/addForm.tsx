1704db1ad71e9fa47ffa50bdf094e376b4b6fbc2
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-19 15:15:44 +0700

    Add item

--- app/_components/addPostForm/addForm.tsx
+++ app/_components/addPostForm/addForm.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useFormState } from 'react-dom'
+import { addPost } from '@/app/_lib/data';
+
 export const AddPostForm = () => {
     
     // This for, will be linked to the method addPost, and it's initial state is undefined
     // The method addPost will be created inside lib/data.ts
-
     const [state, formAction] = useFormState(addPost, undefined);
   return (
     <form action={formAction}>
@@ -15,6 +16,7 @@ export const AddPostForm = () => {
         <input type="text" name="img" placeholder='Image URL' />
         <textarea name="desc" id="" cols={30} rows={10} ></textarea>
         <button>Add</button>
+        {state?.error}
     </form>
   )
 }
