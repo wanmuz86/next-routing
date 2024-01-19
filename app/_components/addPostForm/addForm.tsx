35866600067d739f51f7299f8edab77f9e8eebe5
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-19 15:07:19 +0700

    Add form compoenent

---
+++ app/_components/addPostForm/addForm.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useFormState } from 'react-dom'
+export const AddPostForm = () => {
+    
+    // This for, will be linked to the method addPost, and it's initial state is undefined
+    // The method addPost will be created inside lib/data.ts
+
+    const [state, formAction] = useFormState(addPost, undefined);
+  return (
+    <form action={formAction}>
+    
+        <input type="hidden" name="userId" value={1} />
+        <input type="text" name="title" placeholder='Enter title' />
+        <input type="text" name="slug" placeholder='Enter slug' />
+        <input type="text" name="img" placeholder='Image URL' />
+        <textarea name="desc" id="" cols={30} rows={10} ></textarea>
+        <button>Add</button>
+    </form>
+  )
+}
