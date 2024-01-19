b3ebb78cd4381baf78d72a0dbee616d0e3a9eb84
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-19 15:18:17 +0700

    add admin page

---
+++ app/admin/posts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AddPostForm } from '@/app/_components/addPostForm/addForm'
+
+export default function AdminPage() {
+  return (
+    <div>
+        <h1>Add new Post</h1>
+        <AddPostForm/>
+    </div>
+  )
+}
