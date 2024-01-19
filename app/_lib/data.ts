7c506a946faf8eb890740f77c3b9c2dae021fb16
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-19 15:13:58 +0700

    Add item

--- app/_lib/data.ts
+++ app/_lib/data.ts
@@ -1,4 +1,5 @@
 import {Post} from '@/app/_lib/models'
+import { revalidatePath } from 'next/cache';
 
 export const getPosts = async () => {
     try {
@@ -10,3 +11,32 @@ export const getPosts = async () => {
     }
    };
    
+
+export const addPost = async (prevState, formData)=> {
+    
+    const { title, desc, slug, userId } = Object.fromEntries(formData);
+
+    try {
+        const newPost = new Post(
+            {
+                title,
+                desc,
+                slug,
+                userId
+            }
+        );
+        await newPost.save();
+        console.log("Saved to db");
+        /// RevalidatePath -> Clear/Purge the cache 
+
+        // Impacted if I get the posts directly from data.ts (2nd way)
+        revalidatePath("/posts")
+        // Impacted if I get the posts through the route handler
+        revalidatePath("/api/posts")
+
+    } catch (err){
+        console.log(err);
+        return {error:"Something is wrong"}
+    }
+
+}
