4b1c1d796b7812a808aa1a326fa8a318133068f7
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-19 14:23:04 +0700

    code without interface

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PostCard from '../_components/post_card/postCard';
 const getData = async () => {
-    const res = await fetch("/api/posts");
+    const res = await fetch("http://localhost:3000/api/posts");
     if (!res.ok){
         throw new Error("Something is wrong")
     }
