cc547a7d3cca98622cb745ffb111390e75dfff41
Wan Muzaffar <wanmuz@Wans-MacBook-Air.local>
2024-01-19 08:59:47 +0700

    add fetch api on products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,30 +2,55 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image';
 
-export default function Product() {
+interface Product {
+  id:number,
+  title:string
+}
+
+const getData = async () => {
+  const res = await fetch("https://dummyjson.com/products");
+  if (!res.ok) {
+    throw new Error("Something is wrong")
+
+  }
+  return res.json();
+}
+
+// Change to arrow notification so I can call async await
+const Product = async () => {
+  const products = await getData();
   return (
     <div>
       <Link href="/">Home</Link>
       <h1>Product page</h1>
       <p>This is product listing page</p>
+
       <Image
-        src="/images/iklan.jpeg"
-        height={300}
-        width={300}
-        alt='Ads on product'
-      />
-      <Image
-      src="https://anak2u.s3-ap-southeast-1.amazonaws.com/explore/logo/wonders-year-preschool.jpg"
-      height={400}
-      width={400}
-      alt="School a"
+        src="https://anak2u.s3-ap-southeast-1.amazonaws.com/explore/logo/wonders-year-preschool.jpg"
+        height={400}
+        width={400}
+        alt="School a"
       />
       <ul>
+        {
+          products.map((val : Product) => {
+            return (
+              <li>
+                <Link href={`products/${val.id}`}>
+                  {val.title}
+                </Link></li>
+            )
+          }
+          )
+        }
+      </ul>
+      {/* <ul>
         <li> <Link href="products/1">Product 1</Link></li>
         <li><Link href="products/2">Product 2</Link></li>
         <li> <Link href="products/3">Product 3</Link></li>
       </ul>
-
+*/}
     </div>
   )
 }
+export default Product;
